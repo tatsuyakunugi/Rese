5809ed8d3699b63ace26015f40735a46891be5e1
tatsuya <quercus.acutissima.tree@gmail.com>
2024-05-28 17:58:09 +0900

    記述追加

--- src/app/Http/Controllers/ShopController.php
+++ src/app/Http/Controllers/ShopController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Genre;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $shops = Shop::all();
+        return view('index', compact('shops'));
     }
 
     public function detail()
