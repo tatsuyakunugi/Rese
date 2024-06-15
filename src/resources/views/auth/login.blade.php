88aa7f769c60e9113452edeac7cc719f2ad5e244
tatsuya <quercus.acutissima.tree@gmail.com>
2024-06-15 13:07:14 +0900

    記述修正

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -16,49 +16,53 @@
     </header>
     <main class="main">
         <div class="login__content">
-            <form class="login-form" action="/login" method="post">
-                @csrf
-                <div class="login-form__heading">
-                    <p>Login</p>
-                </div>
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">Email</span>
+            <div class="login__content-card">
+                <form class="login-form" action="/login" method="post">
+                    @csrf
+                    <div class="login-form__heading">
+                        <p class="login-form__heading--title">Login</p>
                     </div>
-                    <div class="form__group-content">
-                        <div class="form__input--text">
-                            <input type="email" name="email">
+                    <div class="login-form__items">
+                        <div class="form__group">
+                            <div class="form__group-title">
+                                <span class="form__label--item">Email</span>
+                            </div>
+                            <div class="form__group-content">
+                                <div class="form__input--text">
+                                    <input type="email" name="email">
+                                </div>
+                            </div>
+                        </div>
+                        @error('email')
+                        <div class="form__error">
+                            <p class="form__error-message">
+                                {{ $errors->first('email') }}
+                            </p>
+                        </div>
+                        @enderror
+                        <div class="form__group">
+                            <div class="form__group-title">
+                                <span class="form__label--item">Password</span>
+                            </div>
+                            <div class="form__group-content">
+                                <div class="form__input--text">
+                                    <input type="password" name="password">
+                                </div>
+                            </div>
+                        </div>
+                        @error('password')
+                        <div class="form__error">
+                            <p class="form__error-message">
+                                {{ $errors->first('password') }}
+                            </p>
+                        </div>
+                        @enderror
+                        <div class="form__button">
+                            <button class="form__button-submit" type="submit">ログイン</button>
                         </div>
                     </div>
-                </div>
-                @error('email')
-                <div class="form__error">
-                    <p class="form__error-message">
-                        {{ $errors->first('email') }}
-                    </p>
-                </div>
-                @enderror
-                <div class="form__group">
-                    <div class="form__group-title">
-                        <span class="form__label--item">Password</span>
-                    </div>
-                    <div class="form__group-content">
-                        <div class="form__input--text">
-                            <input type="password" name="password">
-                        </div>
-                    </div>
-                </div>
-                @error('password')
-                <div class="form__error">
-                    <p class="form__error-message">
-                        {{ $errors->first('password') }}
-                    </p>
-                </div>
-                @enderror
-                <div class="form__button">
-                    <button class="form__button-submit" type="submit">ログイン</button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </main>
 </body>
