5403d916150fdc6c9d17b295133bb7a10d7d2e82
tatsuya <quercus.acutissima.tree@gmail.com>
2024-06-29 17:47:48 +0900

    アイコン追加

--- src/resources/views/list.blade.php
+++ src/resources/views/list.blade.php
@@ -7,12 +7,13 @@
     <title>Rese</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/list.css') }}" />
+    <link href="https://use.fontawesome.com/releases/v6.5.2/css/all.css" rel="stylesheet">
 </head>
 <body>
     <header class="header">
         <div class="header__inner">
             <div class="header-item">
-                <a class="header__logo" href="/menu">Rese</a>
+                <a class="header__logo" href="/menu"><i class="fa-solid fa-square-poll-horizontal"></i>Rese</a>
             </div>
         </div>
     </header>
