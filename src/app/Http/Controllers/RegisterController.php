4de1a217ac09161cf8b9e9ec72b6a58573e8e13b
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-09 17:35:15 +0900

    メール認証アクション追加

--- src/app/Http/Controllers/RegisterController.php
+++ src/app/Http/Controllers/RegisterController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use App\Mail\EmailVerification;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
+use Carbon\Carbon;
 
 class RegisterController extends Controller
 {
@@ -14,27 +20,51 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function postRegister(RegisterRequest $request)
+    protected function validator(array $data)
     {
-        //バリテーション
-        $this->validate($request,[
+        return Validator::make($data, [
             'name' => 'required',
-            'email' => 'email|required|unique:users',
+            'email' => 'required|string|email|unique:users',
             'password' => 'required|min:8',
         ]);
+    }
 
-        //DBインサート
-        $user = new User([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
+    protected function create(array $data)
+    {
+        $this->validator($data)->validate();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'email_verify_token' => base64_encode($data['email']),
         ]);
 
-        //保存
-        $user->save();
+        $email = new EmailVerification($user);
+        Mail::to($user->email)->send($email);
 
-        //リダイレクト
-        return redirect('thanks');
+        return $user;
+    }
+
+    public function postRegister(RegisterRequest $request)
+    {
+        event(new Registered($user = $this->create( $request->all() )));
+
+        return view('auth.registered');
+    }
+
+    public function verify($email_token)
+    {
+        $user = User::where('email_verify_token', $email_token)->first();
+
+        if(!$user)
+        {
+            return view('thanks')->with('message', '無効なトークンです。');
+        }elseif($user->email_verified){
+            return view('thanks')->with('message', 'すでに本登録されています。ログインして利用してください。');
+        }else{
+            $user->verified();
+            return view('thanks')->with('message', 'ご登録ありがとうございました。ログインして利用してください。');
+        }   
     }
 
     public function thanks()
