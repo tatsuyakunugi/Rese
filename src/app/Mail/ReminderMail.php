b1dc941917d5de089226ecc889e7f1b36d80f956
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-10 17:17:15 +0900

    リマインドメール内容作成

---
+++ src/app/Mail/ReminderMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ReminderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.reminder')
+        ->with(['reservation'=>$this->reservation])->subject('【Rese】ご予約内容の確認');
+    }
+}
