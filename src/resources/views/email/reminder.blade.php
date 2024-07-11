4a72268e06bf3459b2cdb34e19c6fe2f6e4cfa02
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-11 17:45:57 +0900

    リマインドメール文面修正

--- src/resources/views/email/reminder.blade.php
+++ src/resources/views/email/reminder.blade.php
@@ -5,7 +5,7 @@
 <br>
 ＜ご予約情報＞<br>
 店名： {{$reservation->shop->shop_name}}<br>
-ご予約日時： {{ $reservation->reservation_day}}<br>
+ご予約日時： {{ $reservation->reservation_datetime}}<br>
 ご来店人数： {{ $reservation->number_of_people}}<br>
 <br>
 ご来店お待ちしております。
