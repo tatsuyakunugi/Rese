31bc2f57d02a0d3c801b9c387815eec54cfc77ba
tatsuya <quercus.acutissima.tree@gmail.com>
2024-07-12 18:50:54 +0900

    記述修正

--- src/resources/views/auth/registered.blade.php
+++ src/resources/views/auth/registered.blade.php
@@ -1,44 +1,28 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Rese</title>
-    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/registered.css') }}" />
-    <link href="https://use.fontawesome.com/releases/v6.5.2/css/all.css" rel="stylesheet">
-</head>
-<body>
-    <header class="header">
-        <div class="header__inner">
-            <a class="header__icon" href="/menu"><i class="fa-solid fa-square-poll-horizontal"></i></a>
-            <h2 class="header__logo">Rese</h2>
-        </div>
-    </header>
-    <main class="main">
-        <div class="registered__content">
-            <div class="registered-card">
-                <div class="registered-card__content">
-                    <div class="registered-card__header">
-                        仮会員登録完了
-                    </div>
-                    <div class="registered-card__body">
-                        <p>
-                            この度は、ご登録いただき、誠にありがとうございます。
-                        </p>
-                        <p>
-                            ご本人様確認のため、ご登録いただいたメールアドレスに、<br>
-                            本登録のご案内のメールが届きます。
-                        </p>
-                        <p>
-                            そちらに記載されているURLにアクセスし、<br>
-                            アカウントの本登録を完了させてください。
-                        </p>
-                    </div>
-                </div>
+@extends('layouts.app')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/registered.css') }}" />
+@endsection
+@section('content')   
+<div class="registered__content">
+    <div class="registered-card">
+        <div class="registered-card__content">
+            <div class="registered-card__header">
+                仮会員登録完了
+            </div>
+            <div class="registered-card__body">
+                <p>
+                    この度は、ご登録いただき、誠にありがとうございます。
+                </p>
+                <p>
+                    ご本人様確認のため、ご登録いただいたメールアドレスに、<br>
+                    本登録のご案内のメールが届きます。
+                </p>
+                <p>
+                    そちらに記載されているURLにアクセスし、<br>
+                    アカウントの本登録を完了させてください。
+                </p>
             </div>
         </div>
-    </main>
-</body>
-</html>
+    </div>
+</div>
+@endsection
