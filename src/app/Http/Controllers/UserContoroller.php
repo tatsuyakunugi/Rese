4bd9a140728cec1854fe08aa939140a25d39a1f6
tatsuyakunugi <quercus.acutissima.tree@gmail.com>
2024-11-06 22:31:49 +0900

    use文修正

--- src/app/Http/Controllers/UserContoroller.php
+++ src/app/Http/Controllers/UserContoroller.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 use App\Models\Shop;
 use App\Models\Area;
 use App\Models\Genre;
