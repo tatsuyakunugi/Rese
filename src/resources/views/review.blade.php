500473cf2ad4e705096346234b37a707d1e311a4
tatsuyakunugi <quercus.acutissima.tree@gmail.com>
2024-11-20 09:22:23 +0900

    入力フォームデザイン修正

--- src/resources/views/review.blade.php
+++ src/resources/views/review.blade.php
@@ -4,28 +4,28 @@
 @endsection
 @section('content')
 <div class="review__content">
-    <div class="review__content--inner">
-        <div class="shop-card">
-            <div class="shop-card__img">
-                <img src="{{ Storage::url($shop->shop_image_path) }}" alt="">
-            </div>
-            <div class="shop-card__content">
-                <h2 class="shop-name">{{ $shop->shop_name }}</h2>
-                <div class="shop-card__content-tag">
-                    <p class="shop-area">#{{ $shop->area->area_name }}</p>
-                    <p class="shop-genre">#{{ $shop->genre->genre_name }}</p>
+    @if(is_null($review))
+    <form class="review-form" action="{{ route('reviews.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="review-form__inner">
+            <div class="shop-card">
+                <div class="shop-card__img">
+                    <img src="{{ Storage::url($shop->shop_image_path) }}" alt="">
                 </div>
-                <div class="shop-card__items">
-                    <div class="detail__link-button">
-                        <a class="detail__link" href="/detail/{{ $shop->id }}">詳しく見る</a>
+                <div class="shop-card__content">
+                    <h2 class="shop-name">{{ $shop->shop_name }}</h2>
+                    <div class="shop-card__content-tag">
+                        <p class="shop-area">#{{ $shop->area->area_name }}</p>
+                        <p class="shop-genre">#{{ $shop->genre->genre_name }}</p>
+                    </div>
+                    <div class="shop-card__items">
+                        <div class="detail__link-button">
+                            <a class="detail__link" href="/detail/{{ $shop->id }}">詳しく見る</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        @if(is_null($review))
-        <div class="review-card">
-            <form class="review-form" action="{{ route('reviews.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
+            <div class="review-card">
                 <div class="form__group">
                     <div class="form__group--title">
                         <p>体験を評価してください</p>
@@ -55,6 +55,7 @@
                     <div class="input__comment">
                         <textarea name="comment">{{ old('comment') }}</textarea>
                     </div>
+                    <p class="input__comment--info">0/400(最大文字数)</p>
                     <div class="form__error">
                         @error('comment')
                         {{ $message }}
@@ -75,17 +76,36 @@
                         @enderror
                     </div>
                 </div>
-                <div class="review-form__button">
-                    <input type="hidden" name="shop_id" value="{{ $shop->id }}">
-                    <button class="review-form__button-submit" type="submit">口コミを投稿する</button>
-                </div>
-            </form>
+            </div>
         </div>
-        @else
-        <div class="review-card">
-            <form class="review-form" action="{{ route('reviews.update') }}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
+        <div class="review-form__button">
+            <input type="hidden" name="shop_id" value="{{ $shop->id }}">
+            <button class="review-form__button-submit" type="submit">口コミを投稿する</button>
+        </div>
+    </form>
+    @else
+    <form class="review-form" action="{{ route('reviews.update') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="review-form__inner">
+            <div class="shop-card">
+                <div class="shop-card__img">
+                    <img src="{{ Storage::url($shop->shop_image_path) }}" alt="">
+                </div>
+                <div class="shop-card__content">
+                    <h2 class="shop-name">{{ $shop->shop_name }}</h2>
+                    <div class="shop-card__content-tag">
+                        <p class="shop-area">#{{ $shop->area->area_name }}</p>
+                        <p class="shop-genre">#{{ $shop->genre->genre_name }}</p>
+                    </div>
+                    <div class="shop-card__items">
+                        <div class="detail__link-button">
+                            <a class="detail__link" href="/detail/{{ $shop->id }}">詳しく見る</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="review-card">
                 <div class="form__group">
                     <div class="form__group--title">
                         <p>体験を評価してください</p>
@@ -115,6 +135,7 @@
                     <div class="input__comment">
                         <textarea name="comment">{{ old('comment', $review->comment) }}</textarea>
                     </div>
+                    <p class="input__comment--info">0/400(最大文字数)</p>
                     <div class="form__error">
                         @error('comment')
                         {{ $message }}
@@ -135,14 +156,14 @@
                         @enderror
                     </div>
                 </div>
-                <div class="review-form__button">
-                    <input type="hidden" name="shop_id" value="{{ $shop->id }}">
-                    <button class="review-form__button-submit" type="submit">口コミを編集する</button>
-                </div>
-            </form>
+            </div>
         </div>
-        @endif
-    </div>
-    </div>
+        <div class="review-form__button">
+            <input type="hidden" name="shop_id" value="{{ $shop->id }}">
+            <button class="review-form__button-submit" type="submit">口コミを編集する</button>
+        </div>
+    </form>
+    @endif
+</div>
 </div>
 @endsection
